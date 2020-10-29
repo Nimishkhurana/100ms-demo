ba29bb85b8ee77153e7125f7d7dd583a6b5b3d5d
praveen <apnerve@gmail.com>
2020-10-29 13:45:51 +0530

    Show mute/unmute buttons on the video

--- src/videoview/MainVideoView.jsx
+++ src/videoview/MainVideoView.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PinIcon from 'mdi-react/PinIcon';
 import PinOffIcon from 'mdi-react/PinOffIcon';
+import MicrophoneIcon from "mdi-react/MicrophoneIcon"
+import MicrophoneOffIcon from "mdi-react/MicrophoneOffIcon"
 
 class MainVideoView extends React.Component {
   componentDidMount = () => {
@@ -35,15 +37,32 @@ class MainVideoView extends React.Component {
           className={`w-full h-full ${pinned ? '' : ''}`}
         />
         <div className="absolute top-0 right-0 pt-2 w-full flex justify-center items-center">
-          <span className="px-2 py-1 bg-indigo-900 rounded-md text-white inline-block bg-opacity-75">
+        <span
+            className="px-3 bg-indigo-900 rounded-md text-white inline-block bg-opacity-75 h-6 inline-block"
+            style={{ lineHeight: "1.5rem" }}
+          >
             {label || stream.info.name}
           </span>
+          <button
+            onClick={(e) => {
+              console.log(e, "mute/unmute")
+            }}
+            className={`w-6 h-6 bg-opacity-50 rounded flex items-center justify-center mx-1 ${
+              audioEnabled ? "bg-gray-800 hover:bg-indigo-500" : "bg-red-500"
+            }`}
+          >
+            {audioEnabled ? (
+              <MicrophoneIcon className="text-indigo-100 w-4 h-4" />
+            ) : (
+              <MicrophoneOffIcon className="text-red-100 w-4 h-4" />
+            )}
+          </button>
           {onPin && (
             <button
               className="w-6 h-6 bg-gray-800 bg-opacity-50 hover:bg-indigo-500 rounded flex items-center justify-center"
               onClick={onPin}
             >
-              <PinIcon className="w-4 h-4 text-white" />
+              <PinIcon className="w-4 h-4 text-indigo-100" />
             </button>
           )}
           {onUnpin && (
